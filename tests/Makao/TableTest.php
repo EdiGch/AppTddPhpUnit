89ade6ff0cf1893a3526a92a70ad9757f2f055cd
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2021-04-06 18:37:57 +0200

    First class

---
+++ tests/Makao/TableTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Makao;
+
+use PHPUnit\Framework\TestCase;
+
+class TableTest extends TestCase
+{
+
+    public function testShouldCreateEmptyTable()
+    {
+        // Expect
+
+        // Given
+
+        // When
+
+        // Then
+
+
+    }
+
+
+}
